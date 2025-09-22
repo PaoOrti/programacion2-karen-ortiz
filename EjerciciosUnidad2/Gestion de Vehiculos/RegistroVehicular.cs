09145c7cdcf27ab3e1ff048c5dc9b797afbb76f8
Karen Ortiz <103227090+PaoOrti@users.noreply.github.com>
2025-09-21 19:30:47 -0600

    Implementada la clase RegistroVehicular
    Con métodos para agregar vehículos y mostrar impuestos de cada uno"

---
+++ EjerciciosUnidad2/Gestion de Vehiculos/RegistroVehicular.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_Vehiculos
+{
+    public class RegistroVehicular
+    {
+        #region Atributos
+        private List<Vehiculo> listaVehiculos;
+        #endregion
+
+        #region Constructor
+        public RegistroVehicular()
+        {
+            listaVehiculos = new List<Vehiculo>();
+        }
+        #endregion
+
+        #region Métodos
+        public void agregarVehiculo(Vehiculo v)
+        {
+            listaVehiculos.Add(v);
+        }
+
+        public void mostrarImpuestos()
+        {
+            foreach (var vehiculo in listaVehiculos)
+            {
+                Console.WriteLine($"Vehículo: {vehiculo.GetType().Name}, Impuesto: {vehiculo.calcularImpuesto()}");
+            }
+        #endregion
+        }
+    }
+}
