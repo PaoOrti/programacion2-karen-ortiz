e4ccf8c9e67b8e7b9de760f09a9d9c13d0b729ea
Karen Ortiz <103227090+PaoOrti@users.noreply.github.com>
2025-09-21 18:53:57 -0600

    Agregue la Clase Vehículos y sus atributos

---
+++ EjerciciosUnidad2/Gestion de Vehiculos/Vehiculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_Vehiculos
+{
+    public abstract class Vehiculo
+    {
+        #region Atributos
+        protected string marca;
+        protected string modelo;
+        protected int anio;
+        #endregion
+        #region Constructor
+
+        public Vehiculo(string marca, string modelo, int anio)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.anio = anio;
+        }
+        #endregion
+
+        #region Métodos
+        public abstract double calcularImpuesto();
+        #endregion
+    }
+}
