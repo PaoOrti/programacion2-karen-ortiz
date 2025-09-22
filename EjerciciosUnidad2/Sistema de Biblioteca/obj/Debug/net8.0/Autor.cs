9fa1a44aca5dc74b57c30973a6380e7abfb44a27
Karen Ortiz <103227090+PaoOrti@users.noreply.github.com>
2025-09-21 20:39:41 -0600

    Agregue un método a la clase Autor
    Cálculo de edad del autor: El sistema calcula la edad del autor a partir de su fecha de nacimiento.

---
+++ EjerciciosUnidad2/Sistema de Biblioteca/obj/Debug/net8.0/Autor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Biblioteca
+{
+    internal class Autor
+    {
+        // Atributos
+        public string Nombre { get; set; }
+        public string Nacionalidad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        // Constructor
+        public Autor(string nombre, string nacionalidad, DateTime fechaNacimiento)
+        {
+            Nombre = nombre;
+            Nacionalidad = nacionalidad;
+            FechaNacimiento = fechaNacimiento;
+        }
+
+        // Método: Calcular la edad del autor
+        public int GetEdad()
+        {
+            int edad = DateTime.Now.Year - FechaNacimiento.Year;
+            if (FechaNacimiento > DateTime.Now.AddYears(-edad)) edad--;
+            return edad;
+        }
+    }
+
+}
