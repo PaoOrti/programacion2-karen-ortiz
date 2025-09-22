fdc9e405880fdd9272ed4a5063ca0d1c0cc31d3e
Karen Ortiz <103227090+PaoOrti@users.noreply.github.com>
2025-09-21 20:41:57 -0600

    Agregue la relación de composición con la clase Autor
    Con el método:
    MostrarInformacion(): Muestra la información del libro y del autor.

---
+++ EjerciciosUnidad2/Sistema de Biblioteca/obj/Debug/net8.0/Libro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Biblioteca
+{
+    internal class Libro
+    {
+        // Atributos
+        public string Titulo { get; set; }
+        public string ISBN { get; set; }
+        public int AnioPublicacion { get; set; }
+        public Autor Autor { get; set; }  // Composición con la clase Autor
+
+        // Constructor
+        public Libro(string titulo, string isbn, int anioPublicacion, Autor autor)
+        {
+            Titulo = titulo;
+            ISBN = isbn;
+            AnioPublicacion = anioPublicacion;
+            Autor = autor;
+        }
+
+        // Método: Mostrar información del libro y del autor
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Título: {Titulo}");
+            Console.WriteLine($"ISBN: {ISBN}");
+            Console.WriteLine($"Año de Publicación: {AnioPublicacion}");
+            Console.WriteLine($"Autor: {Autor.Nombre}");
+            Console.WriteLine($"Edad del Autor: {Autor.GetEdad()} años");
+        }
+    }
+}
