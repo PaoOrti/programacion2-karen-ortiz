a76adf8b0482671df1841bf7b3cf215dad4d0c35
Karen Ortiz <103227090+PaoOrti@users.noreply.github.com>
2025-09-24 20:02:52 -0600

    Se implementó un constructor para inicializar los atributos básicos.

---
+++ ExamenParcial/Persona.cs
@@ -0,0 +1,26 @@
+﻿public class Persona
+{
+    #region Atributos
+    public string Nombre { get; set; }  // Atributo: nombre de la persona
+    public string Dpi { get; set; }     // Atributo: DPI de la persona
+    public string Correo { get; set; }  // Atributo: correo de la persona
+    #endregion
+
+    #region Métodos
+    // Método para mostrar la información de la persona
+    public virtual void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {Nombre}, DPI: {Dpi}, Correo: {Correo}");
+    }
+    #endregion
+
+    #region Constructor
+    // Constructor para inicializar los atributos de la persona
+    public Persona(string nombre, string dpi, string correo)
+    {
+        Nombre = nombre;
+        Dpi = dpi;
+        Correo = correo;
+    }
+    #endregion
+}
