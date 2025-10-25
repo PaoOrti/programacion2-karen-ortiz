d360e8cfa1d8ba9bf2c5e491ecd6dae579eaff63
Karen Ortiz <103227090+PaoOrti@users.noreply.github.com>
2025-10-24 18:52:25 -0600

    Filtrar una lista de números para obtener los pares

---
+++ Unidad III/GestionAcademica/LINQ Filtrar/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LINQ_Filtrar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear una lista de enteros
+            List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6};
+
+            // Usar LINQ para filtrar solo los números pares
+            var numerosPares = from num in numeros
+                               where num % 2 == 0
+                               select num;
+
+            // Mostrar los resultados
+            Console.WriteLine("Números pares:");
+            foreach (var num in numerosPares)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
