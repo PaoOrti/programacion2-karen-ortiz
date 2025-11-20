4af3b044567c61dde59aca72582e013bf9164082
Karen Ortiz <103227090+PaoOrti@users.noreply.github.com>
2025-11-19 23:18:50 -0600

    Parte 5-10

---
+++ Tutorial ASP.NET Web/ParteFinal/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿UPDATE Movie SET Title = 'Unknown Title' WHERE Title IS NULL;
+UPDATE Movie SET Genre = 'Unknown' WHERE Genre IS NULL;
+UPDATE Movie SET Rating = 'NR' WHERE Rating IS NULL;
+
+SELECT * FROM Movie WHERE Title IS NULL OR Genre IS NULL OR Rating IS NULL;
+
